6f57a8ce3a2447ec04637f26c6ff20d7fdbdbf30
Marco Vela <36453977+characat0@users.noreply.github.com>
2024-10-27 13:54:10 -0500

    dependencies for simulations

---
+++ lib/MovingMNISTConvLSTM/scripts/simulate.jl
@@ -0,0 +1,10 @@
+using DrWatson
+@quickactivate
+
+# using Lux, Random, Optimisers, Zygote, CUDA, LuxCUDA, NPZ, MLUtils, Printf, ProgressMeter, MLFlowClient, JLD2, Statistics, Plots, Dates, ConvLSTM, Accessors
+using NPZ, MLUtils, JLD2
+using ConvLSTM, Lux, CUDA, LuxCUDA
+using ProgressMeter, MLFlowClient, Plots, Dates
+
+
+
