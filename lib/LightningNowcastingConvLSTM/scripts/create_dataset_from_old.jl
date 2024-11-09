f7e22464ec0cfd9dc7660e8b86fae63c67b355dd
Marco Vela <36453977+characat0@users.noreply.github.com>
2024-11-09 09:04:32 -0500

    use gaussian kernel for training set

--- lib/LightningNowcastingConvLSTM/scripts/create_dataset_from_old.jl
+++ lib/LightningNowcastingConvLSTM/scripts/create_dataset_from_old.jl
@@ -2,6 +2,7 @@ using DrWatson
 @quickactivate
 
 using HDF5, JLD2, MLUtils, Random, CodecZlib
+using ImageFiltering
 
 fed = h5read(datadir("exp_raw", "dataset.h5"), "FED")
 lat = h5read(datadir("exp_raw", "dataset.h5"), "lat")
@@ -14,9 +15,21 @@ fed = permutedims(fed[:, :, 1, :, :], (1, 2, 4, 3)) # WxHxTxN
 
 train, val = splitobs(fed; at=.97)
 
-function augment(rng, ds, n_empty=0)
+function uint8_filter(chunk, k)
+    chunk = chunk / Float32(255)
+    res = imfilter(chunk, k)
+    res .= max.(chunk, res)
+    ceil.(UInt8, res * Float32(255))
+end
+
+function augment(rng, ds, n_empty=0, gaussian_filter=false)
     n = size(ds, 4)
     output = zeros(eltype(ds), size(ds)[1:3]..., size(ds, 4)*4 + n_empty)
+    if gaussian_filter
+        @info "applying gaussian filter"
+        k = Float32.(Kernel.gaussian(.9))
+        ds = mapslices(Base.Fix2(uint8_filter, k), ds, dims=(1, 2))
+    end
     idx = shuffle(rng, axes(output, 4))
     for i in 0:3
         @info "rotating $i"
@@ -25,7 +38,7 @@ function augment(rng, ds, n_empty=0)
     output
 end
 
-dataset = augment(Xoshiro(42), train, 2_000)
+dataset = augment(Xoshiro(42), train, 2_000, true)
 
 @info "number of samples for train: $(size(dataset, 4))"
 
