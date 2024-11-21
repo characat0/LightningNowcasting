2ef84ab2fa804dc90358315593d3187db7167d7f
Marco Vela <36453977+characat0@users.noreply.github.com>
2024-11-20 22:57:21 -0500

    write to bashrc

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -12,38 +12,30 @@ GH_USER="gh"
 
 # Update and install dependencies
 echo "Updating system and installing dependencies..."
-sudo apt update
-sudo apt install -y curl tar wget coreutils git
+sudo apt-get update
+sudo apt-get install -y curl tar wget coreutils git
 
 # Remove tmux
 touch ~/.no_auto_tmux;
 
-# Create the 'gh' user if it doesn't exist
-if ! id -u "$GH_USER" &>/dev/null; then
-    echo "Creating user '$GH_USER'..."
-    sudo useradd --create-home --shell /bin/bash "$GH_USER"
-else
-    echo "User '$GH_USER' already exists."
-fi
 
 # Create runner directory
 echo "Setting up runner directory..."
 mkdir -p "$RUNNER_DIR"
-chown "$GH_USER:$GH_USER" "$RUNNER_DIR"
 cd "$RUNNER_DIR"
 
 # Download the runner package
 echo "Downloading GitHub Actions runner version $RUNNER_VERSION..."
-sudo -u "$GH_USER" curl -o "$RUNNER_ARCH" -L "$RUNNER_URL"
+curl -o "$RUNNER_ARCH" -L "$RUNNER_URL"
 echo "93ac1b7ce743ee85b5d386f5c1787385ef07b3d7c728ff66ce0d3813d5f46900  $RUNNER_ARCH" | shasum -a 256 -c
-sudo -u "$GH_USER" tar xzf "$RUNNER_ARCH"
+tar xzf "$RUNNER_ARCH"
 
 # Setup the runner package
 echo "https://github.com/$GITHUB_REPOSITORY"
-sudo -u "$GH_USER" ./config.sh --unattended --url https://github.com/$GITHUB_REPOSITORY --token $GITHUB_ACTIONS_TOKEN --labels gpu --ephemeral --disableupdate
+./config.sh --unattended --url https://github.com/$GITHUB_REPOSITORY --token $GITHUB_ACTIONS_TOKEN --labels gpu --ephemeral --disableupdate
 
-sudo -u "$GH_USER" echo "export PATH=\$PATH:/usr/local/julia/bin/" >> ~/.bashrc
-sudo -u "$GH_USER" echo "export JULIA_NUM_THREADS=auto" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/local/julia/bin/" >> ~/.bashrc
+echo "export JULIA_NUM_THREADS=auto" >> ~/.bashrc
 
 echo $(realpath ~/.bashrc)
 cat ~/.bashrc
@@ -52,38 +44,5 @@ cat ~/.bashrc
 touch /root/READY
 
 # Setup the runner package
-sudo -u "$GH_USER" ./run.sh
-
-
-# repo=$(echo ${REPO_NAME} | cut -d'/' -f2)
-
-# useradd -m gh
-
-
-
-# curl -o actions-runner-osx-x64-2.320.0.tar.gz -L
-
-# while :; do
-#   # Run sha256sum and capture the output
-#   output=$(cd ${repo}/lib/${SUBPACKAGE} && sha256sum -c data/SHA256SUMS 2>&1)
-
-#   # Check if the output contains "FAILED open" or "FAILED read"
-#   if echo "$output" | grep -qE "FAILED open or read"; then
-#     echo "Some files are not yet downloaded. Waiting 1 minute..."
-#     sleep 60
-#   else
-#     echo "All files verified successfully!"
-#     break
-#   fi
-# done
-
-# cd ${repo}/lib/${SUBPACKAGE} && chmod +x ./scripts/train.sh  && ./scripts/train.sh
-
-# if [ $? -eq 0 ]; then
-#     touch ~/SUCCESS
-#     echo "Command succeeded. ~/SUCCESS created."
-# else
-#     touch ~/FAILED
-#     echo "Command failed. ~/FAILED created."
-# fi
+RUNNER_ALLOW_RUNASROOT=1 ./run.sh
 
