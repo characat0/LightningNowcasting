ce81a1051ba92f97229777a51efeaf375debc7f6
Marco Vela <36453977+characat0@users.noreply.github.com>
2024-12-01 21:57:03 -0500

    overwrite files

--- lib/LightningNowcastingConvLSTM/scripts/prepare.sh
+++ lib/LightningNowcastingConvLSTM/scripts/prepare.sh
@@ -5,8 +5,8 @@ mkdir -p data/exp_pro
 
 BASE_URL=static.unraid.local:8043
 
-wget --no-show-progress ${BASE_URL}/datasets/nowcasting.h5 -P data/exp_raw
-wget --no-show-progress ${BASE_URL}/datasets/train.jld2 -P data/exp_pro
-wget --no-show-progress ${BASE_URL}/datasets/val.jld2 -P data/exp_pro
+curl -o data/exp_raw/nowcasting.h5 ${BASE_URL}/datasets/nowcasting.h5
+curl -o data/exp_pro/train.jld2 ${BASE_URL}/datasets/train.jld2
+curl -o data/exp_pro/val.jld2 ${BASE_URL}/datasets/val.jld2
 
 sha256sum -c data/SHA256SUMS
