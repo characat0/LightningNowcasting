06215458728377465b50d20118b7fc59da4f676b
Marco Vela <36453977+characat0@users.noreply.github.com>
2025-01-12 23:38:19 -0500

    clipnorm

--- lib/LightningNowcastingConvLSTM/scripts/simulate.jl
+++ lib/LightningNowcastingConvLSTM/scripts/simulate.jl
@@ -203,7 +203,7 @@ function simulate(
     rng = Xoshiro(seed)
     d_rng = Dict(["rng.$(k)" => v for (k, v) in struct_to_dict(rng)])
     ps, st = Lux.setup(rng, model) |> dev
-    opt = RMSProp(; eta, rho)
+    opt = OptimiserChain(ClipNorm(), RMSProp(; eta, rho))
     logparam(mlf, run_info, Dict(
         "model.mode" => mode,
         "model.depth" => length(hidden),
